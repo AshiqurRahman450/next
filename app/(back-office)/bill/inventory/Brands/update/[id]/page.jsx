aca1a8ec68b3cfd1e6c23a13502b9338dcf9c0d7
AshiqurRahman450 <asksarkar0786@gmail.com>
2024-12-10 14:01:49 +0530

    Update message

--- app/(back-office)/bill/inventory/Brands/update/[id]/page.jsx
+++ app/(back-office)/bill/inventory/Brands/update/[id]/page.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import NewBrand from '../../New/page'
-import { getData } from '@/lib/getData'
+import React from 'react';
+import NewBrand from '../../New/page'; // Ensure the import path is correct
+import { getData } from '@/lib/getData';
 
-export default  async function Update({params:{id}}) {
-  const data = await getData(`brands/${id}`)
-  console.log(data)
+// Fetch data on the server side using getServerSideProps
+export async function getServerSideProps(context) {
+  const { id } = context.params;
+  const data = await getData(`brands/${id}`);
+  return {
+    props: {
+      initialData: data,
+      isUpdate: true,
+    },
+  };
+}
+
+export default function Update({ initialData, isUpdate }) {
   return (
-    <NewBrand initialData={data} isUpdate={true}/>
-  )
+    <NewBrand initialData={initialData} isUpdate={isUpdate} />
+  );
 }
