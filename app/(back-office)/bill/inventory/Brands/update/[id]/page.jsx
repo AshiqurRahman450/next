1668bacfee6fd8c34d70192e7f6f9c86eecf0d75
AshiqurRahman450 <asksarkar0786@gmail.com>
2024-12-10 12:53:24 +0530

    Update message

--- app/(back-office)/bill/inventory/Brands/update/[id]/page.jsx
+++ app/(back-office)/bill/inventory/Brands/update/[id]/page.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import NewBrand from '../../New/page'
-import { getData } from '@/lib/getData'
+import React from 'react';
+import NewBrand from '../../New/page';
+import { getData } from '@/lib/getData';
 
-export default  async function Update({params:{id}}) {
-  const data = await getData(`brands/${id}`)
-  console.log(data)
-  return (
-    <NewBrand initialData={data} isUpdate={true}/>
-  )
+export default async function Update({ params: { id } }) {
+  const data = await getData(`brands/${id}`);
+  console.log(data);
+
+  return <NewBrand initialData={data} isUpdate={true} />;
 }
